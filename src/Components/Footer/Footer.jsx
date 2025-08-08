100fc64de0f5bd07dddfcbd4e9dd6ea0557c9da0
SaGerz <genarosamuel24@gmail.com>
2025-08-08 21:43:55 +0700

    fix: fix footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="ml-64 bg-gray-100 text-center py-4 text-gray-500 mt-auto">
-      © 2025 ERP Dashboard. All rights reserved.
+      © 2025 Task Management Web APP. All rights reserved.
     </footer>
   );
 }
